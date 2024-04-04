381e29e8d7f654632b0b0e3d00f6edaad1e934fe
Gareth Powell <gareth@ziniki.org>
2024-04-04 21:23:59 +0100

    Supported opening/closing in mobile mode

--- grammar.js
+++ grammar.js
@@ -1,20 +1,17 @@
-// Two things to sort out:
-// how to make this load itself
 // how to find a node with the appropriate grammar-section using querySelector
 
 function callonload() {
   hideAll();
   showJust("Files");
+  showHashLocation();
   
+  var hamdiv = document.getElementsByClassName("hamburger-div")[0];
   var lnks = document.getElementsByClassName("hamburger-section-link");
   for (var i=0;i<lnks.length;i++) {
-    lnks[i].addEventListener('click', clickOnLink());
+    lnks[i].addEventListener('click', clickOnLink(hamdiv));
   }
   
-  var hamdiv = document.getElementsByClassName("hamburger-div");
-  for (var i=0;i<hamdiv.length;i++) {
-    hamdiv[i].addEventListener('click', toggleHamburger());
-  }
+  hamdiv.addEventListener('click', toggleHamburgerEvent(hamdiv));
 }
 
 function hideAll() {
@@ -28,45 +25,58 @@ function showJust(name) {
   // should be able to use a query selector in place of this :-(
   var coll = document.getElementsByClassName("grammar-section");
   for (var i=0;i<coll.length;i++) {
-    if (coll[i].dataset.grammarSection == name)
+    if (coll[i].dataset.grammarSection.toLowerCase() == name.toLowerCase())
       coll[i].classList.remove("hidden");
   }
 }
 
-function clickOnLink() {
+function clickOnLink(hamdiv) {
   return function(ev) {
     var div = ev.target;
     hideAll();
     showJust(div.dataset.grammarSection);
+    toggleHamburger(hamdiv);
+    history.pushState({}, div.dataset.grammarSection, '#' + div.dataset.grammarSection);
   }
 }
 
-function toggleHamburger() {
+function toggleHamburgerEvent(hamdiv) {
   return function(ev) {
-    var gc = document.getElementsByClassName("grammar-content");
-    var hm = document.getElementsByClassName("hamburger-menu");
-    if (ev.target.classList.contains("menu-open")) {
-      ev.target.classList.remove("menu-open");
-      for (var i=0;i<gc.length;i++) {
-        gc[i].classList.remove("menu-open");
-      }
-      for (var i=0;i<hm.length;i++) {
-        hm[i].classList.remove("menu-open");
-      }
-    } else {
-      ev.target.classList.add("menu-open");
-      for (var i=0;i<gc.length;i++) {
-        gc[i].classList.add("menu-open");
-      }
-      for (var i=0;i<hm.length;i++) {
-        hm[i].classList.add("menu-open");
-      }
+    toggleHamburger(hamdiv);
+  }
+}
+function toggleHamburger(hamdiv) {
+  var gc = document.getElementsByClassName("grammar-content");
+  var hm = document.getElementsByClassName("hamburger-menu");
+  if (hamdiv.classList.contains("menu-open")) {
+    hamdiv.classList.remove("menu-open");
+    for (var i=0;i<gc.length;i++) {
+      gc[i].classList.remove("menu-open");
+    }
+    for (var i=0;i<hm.length;i++) {
+      hm[i].classList.remove("menu-open");
+    }
+  } else {
+    hamdiv.classList.add("menu-open");
+    for (var i=0;i<gc.length;i++) {
+      gc[i].classList.add("menu-open");
+    }
+    for (var i=0;i<hm.length;i++) {
+      hm[i].classList.add("menu-open");
     }
   }
 }
 
-// something here should work
-// document.onload = callonload;
-// document.addEventListener('onpageload', onload)
-// window.load = callonload;
+function showHashLocation() {
+  hideAll();
+  if (window.location.hash) {
+    var show = window.location.hash;
+    while (show.charAt(0) == '#' || show.charAt(0) == '/') {
+      show = show.substring(1);
+    }
+    showJust(show);
+  }
+}
+
 window.addEventListener('load', callonload);
+window.addEventListener('popstate', showHashLocation);
